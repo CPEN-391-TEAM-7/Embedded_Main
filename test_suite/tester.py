3ce7204464032624f3560ee19e761a99aac80639
Ali <aliumar18@gmail.com>
2021-02-16 19:33:00 -0800

    Add test for >10 domains

--- test_suite/tester.py
+++ test_suite/tester.py
@@ -16,7 +16,7 @@ sender   = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ####################################################################
 # TEST 1, sending and receing a good single domain
 ####################################################################
-print("Test 1")
+print("TEST 1, sending and receing a good single domain")
 sender.sendto(b"facebook.com",(de1_address,de1_port))
 
 data, addr = receiver.recvfrom(1024) # buffer size is 1024 bytes
@@ -32,7 +32,7 @@ time.sleep(5)
 ####################################################################
 # TEST 2, sending and receing a bad single domain
 ####################################################################
-print("Test 2")
+print("TEST 2, sending and receing a bad single domain")
 sender.sendto(b"badnet.hack",(de1_address,de1_port))
 
 data, addr = receiver.recvfrom(1024) # buffer size is 1024 bytes
@@ -49,7 +49,7 @@ time.sleep(5)
 # TEST 3, sending 2 good domains at once
 ####################################################################
 
-print("Test 3")
+print("TEST 3, sending 2 good domains at once")
 sender.sendto(b"face.com",(de1_address,de1_port))
 sender.sendto(b"book.com",(de1_address,de1_port))
 d1 = True
@@ -74,7 +74,7 @@ time.sleep(5)
 # TEST 4, sending good domain and bad domain at once
 ####################################################################
 
-print("Test 3")
+print("TEST 4, sending good domain and bad domain at once")
 sender.sendto(b"face.com",(de1_address,de1_port))
 sender.sendto(b"book.hack",(de1_address,de1_port))
 d1 = True
@@ -91,6 +91,41 @@ while d1 or d2:
 
     if (response not in ["book.hack1","face.com0"]):
         fail("Unexpected response: " + response)
+
+
+time.sleep(5)
+
+####################################################################
+# TEST 5, sending 10 domains at once
+####################################################################
+
+print("TEST 5, sending 10 domains at once")
+
+domains = ["1.com","2.hack","3.com","4.hack","5.com","6.hack","7.com","8.hack","9.com","10.hack"]
+
+sender.sendto(b"1.com",(de1_address,de1_port))
+sender.sendto(b"2.hack",(de1_address,de1_port))
+sender.sendto(b"3.com",(de1_address,de1_port))
+sender.sendto(b"4.hack",(de1_address,de1_port))
+sender.sendto(b"5.com",(de1_address,de1_port))
+sender.sendto(b"6.hack",(de1_address,de1_port))
+sender.sendto(b"7.com",(de1_address,de1_port))
+sender.sendto(b"8.hack",(de1_address,de1_port))
+sender.sendto(b"9.com",(de1_address,de1_port))
+sender.sendto(b"10.hack",(de1_address,de1_port))
+
+count = 0 
+
+while count < 10:
+    data, addr = receiver.recvfrom(1024) # buffer size is 1024 bytes
+    response = data.decode()
+    print("received message: %s" % response)
+
+    if (response[:-1] in domains):
+        count += 1
+    else:
+        fail("Unexpected: " + response)
+
 
 
 time.sleep(5)
